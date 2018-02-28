795ea7132531c554cc9c86c5847e3c2db5bdb5c6
Won Maung Thein <wonmaungthein@gmail.com>
2018-02-28 16:37:58 +0000

    added two cakes name, img and a button

--- cakelovers/src/App.js
+++ cakelovers/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import CakeList from './Components/CakeList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">We are cake lovers</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <CakeList />        
+          </p>
       </div>
     );
   }
---
+++ cakelovers/src/Components/CakeList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import carrotcake from '../../src/carrotcake.jpg';
+import chocolate from '../../src/chocolate.jpg';
+
+const CakeNames = {
+    'one': 'Carrot Cake',
+    'two': 'Chotolate Cake'
+}
+
+class CakeList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Here is the cake list </h1>
+                <div>
+                    <h3> {CakeNames.one}</h3>
+                    <img src={carrotcake} width="150" height="150" alt="cake" />
+                    <button> Add this cake </button>
+                </div>
+                <div>
+                    <h3> {CakeNames.two}</h3>
+                    <img src={chocolate} width="150" height="150" alt="cake" />
+                    <button> Add this cake </button>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CakeList;
